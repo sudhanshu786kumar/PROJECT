ede878d09298114f7e6a353bb7db0f863fd05ad3
SARTHAK SIKRIWAL <88199443+Sarthakssingh@users.noreply.github.com>
2022-07-27 11:27:29 +0530

    Update Search.js

--- frontend/src/components/Home/Search.js
+++ frontend/src/components/Home/Search.js
@@ -13,31 +13,34 @@
         </h1>
         <div style={searchstyle}>
 
-        const checkList = ["Bengaluru/Banglore", "Delhi/NCR", "Hyderabad", "Mumbai"];
+        const Search = ["Bengaluru/Banglore", "Delhi/NCR", "Hyderabad", "Mumbai"];
 
-        return (
-          <div className="checkList">
-            <div className="title">Location</div>
-            <div className="list-container">
-              {checkList.map((item, index) => (
-                <div key={index}>
-                  <span>{item}</span>
-                </div>
-              ))}
+    return (
+    <div className="Search">
+        <div className="title">Location:</div>
+        <div className="list-container">
+        {Search.map((item, index) => (
+            <div key={index}>
+            <span>{item}</span>
             </div>
-          </div>
-        );
+        ))}
+        </div>
+    </div>
+    );
+    </>
+  )
 
         const checkList = ["0-3 Lakhs", "3-6 Lakhs", "6-10 Lakhs", "10-15 Lakhs"];
 
         return (
-          <div className="checkList">
-            <div className="title">Location</div>
-            <div className="list-container">
-              {checkList.map((item, index) => (
-                <div key={index}>
-                  <span>{item}</span>
-                </div>
+          return (
+    <div className="Search">
+        <div className="title">Salary:</div>
+        <div className="list-container">
+        {Search.map((item, index) => (
+            <div key={index}>
+            <span>{item}</span>
+            </div>
               ))}
             </div>
           </div>
