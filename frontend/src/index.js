1e60d247d0788eae3e2e1368943f082d24f6e8d9
Unnati Moralwar <unnatimoralwar@gmail.com>
2022-07-30 16:58:56 +0530

    demo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import Todo from './Todo'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
- 
-<Todo/>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-
-
+//pull origin
+//git add .
+//git commit -m "df"
