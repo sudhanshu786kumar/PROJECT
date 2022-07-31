3d96cc87308e927bf62df60c77b4ddd826ce0387
Unnati Moralwar <unnatimoralwar@gmail.com>
2022-08-01 00:41:04 +0530

    job deails added

--- backend/Controllers/Admincontrol.js
+++ backend/Controllers/Admincontrol.js
@@ -1,70 +1,102 @@
-const admin = require('../models/admin');
-const formdata=require('../models/AdminForm')
-const bcrypt = require('bcryptjs')
+const admin = require("../models/admin");
+const formdata = require("../models/AdminForm");
+const bcrypt = require("bcryptjs");
 const postData = async (req, res, next) => {
-    const { name, password } = req.body;
-let admins;
-    try {
-         admins = new admin({
-            name,
-            password
-        })
+  const { name, password } = req.body;
+  let admins;
+  try {
+    admins = new admin({
+      name,
+      password,
+    });
 
-        const salt = await bcrypt.genSalt(10);
-        admins.password = await bcrypt.hash(admins.password, salt);
+    const salt = await bcrypt.genSalt(10);
+    admins.password = await bcrypt.hash(admins.password, salt);
 
-
-        admins.save()
-      
-    } catch (error) {
-        console.log(error)
-    }
-    return res.status(201).json((
-        admins
-    ))
-
-}
+    admins.save();
+  } catch (error) {
+    console.log(error);
+  }
+  return res.status(201).json(admins);
+};
 
 const logData = async (req, res) => {
-    const { name, password } = req.body;
-    const admins = await admin.findOne({ name: name });
-    if (admins) {
-        const vpassword = await bcrypt.compare(password, admins.password);
-        if (vpassword) {
-            res.status(200).json({ message: "Log in Success" });
-        } else {
-            res.status(400).json({ error: "Login failed" });
-        }
+  const { name, password } = req.body;
+  const admins = await admin.findOne({ name: name });
+  if (admins) {
+    const vpassword = await bcrypt.compare(password, admins.password);
+    if (vpassword) {
+      res.status(200).json({ message: "Log in Success" });
     } else {
-        res.status(401).json({ error: "User does not exist" });
+      res.status(400).json({ error: "Login failed" });
     }
-
-}
-
+  } else {
+    res.status(401).json({ error: "User does not exist" });
+  }
+};
+const getData = async (req, res) => {
+  let id = req.params.id;
+  let data = await formdata.findById(id);
+  if (!data) {
+    res.status(404).json({
+      msg: "Not Found",
+    });
+  }
+  return res.status(201).json(data);
+};
 const formData = async (req, res) => {
-    const { jobid, jobtitle,
-         date, role, resp, compname,
-          exp, salary, loc, skills, deg, compinfo, emptype,
-           industry, keyword, jobdesc } = req.body;
-    let data;
-    try {
-        data = await new formdata({
-            jobid, jobtitle, date, role, resp, compname, exp, salary, loc, skills, deg, compinfo, emptype, industry, keyword:String(keyword), jobdesc
-        })
-data.save();
-    } catch (error) {
-console.log(error)
-    }
+  const {
+    jobid,
+    jobtitle,
+    date,
+    role,
+    resp,
+    compname,
+    exp,
+    salary,
+    loc,
+    skills,
+    deg,
+    compinfo,
+    emptype,
+    industry,
+    keyword,
+    jobdesc,
+  } = req.body;
+  let data;
+  try {
+    data = await new formdata({
+      jobid,
+      jobtitle,
+      date,
+      role,
+      resp,
+      compname,
+      exp,
+      salary,
+      loc,
+      skills,
+      deg,
+      compinfo,
+      emptype,
+      industry,
+      keyword: String(keyword),
+      jobdesc,
+    });
+    data.save();
+  } catch (error) {
+    console.log(error);
+  }
 
-if(!data){
+  if (!data) {
     res.status(401).json({
-        msg:"error adding"
-    })
-}return res.status(201).json(data)
-}
-
-
+      msg: "error adding",
+    });
+  }
+  return res.status(201).json(data);
+};
 
 exports.postData = postData;
 exports.logData = logData;
-exports.formData=formData;
+exports.formData = formData;
+exports.getData = getData;
--- backend/Routes/Adminroute.js
+++ backend/Routes/Adminroute.js
@@ -1,23 +1,21 @@
-const express=require('express');
-const router=express.Router();
-const schema=require('../models/admin');
-const Controllers=require('../Controllers/Admincontrol');
-const AdminForm = require('../models/AdminForm');
+const express = require("express");
+const router = express.Router();
+const schema = require("../models/admin");
+const Controllers = require("../Controllers/Admincontrol");
+const AdminForm = require("../models/AdminForm");
 
+router.post("/", Controllers.postData);
+router.post("/login", Controllers.logData);
+router.post("/data", Controllers.formData);
+router.get("/job/:id", Controllers.getData);
+router.get("/data", async (req, res) => {
+  let data = await AdminForm.find();
+  if (!data) {
+    res.status(404).json({
+      msg: "no data",
+    });
+  }
+  return res.json(data);
+});
 
-router.post('/',Controllers.postData);
-router.post('/login',Controllers.logData);
-router.post('/data',Controllers.formData);
-router.get('/data',async(req,res)=>{
-    let data= await AdminForm.find()
-    if(!data){
-        res.status(404).json({
-            msg:"no data"
-        })
-    }return res.json(data)
-})
-
-
-
-
-module.exports=router
+module.exports = router;
