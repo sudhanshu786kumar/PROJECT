7eb6364005cfa10174374446d5c99fe3d4b16628
Unnati Moralwar <unnatimoralwar@gmail.com>
2022-07-31 23:09:24 +0530

    demo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,15 @@
-<<<<<<< HEAD
-import React from 'react';
-import {Routes,Route} from 'react-router-dom';
-import Header from "./components/Home/Header"
-import Home from './components/Home/Home';
-import Register from './components/User/Register';
-import Form from './components/Admin/Form';
-import Detail from './components/User/Detail';
-import { useSelector } from 'react-redux';
-import Education from './components/User/Education';
-import Employment from './components/User/Employment';
-import Admin from './components/Admin/Admin.js'
-import Personal from './components/User/Personal'
-=======
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Home/Header";
 import Home from "./components/Home/Home";
 import Register from "./components/User/Register";
-import Form from "./components/Admin/Form";
+//import Form from "./components/Admin/Form";
 import Detail from "./components/User/Detail";
 import { useSelector } from "react-redux";
 import Education from "./components/User/Education";
 import Employment from "./components/User/Employment";
-
->>>>>>> 877c2a09935b9b92b7a10225ddf60a76a5102963
+import Admin from "./components/Admin/Admin.js";
+import Personal from "./components/User/Personal";
 const App = () => {
   const isLoggedIn = useSelector((state) => state.isLoggedIn);
   console.log(isLoggedIn);
@@ -34,23 +20,13 @@ const App = () => {
       </header>
       <main>
         <Routes>
-<<<<<<< HEAD
-            <Route path='/' element={<Home/>} exact/>
-            <Route path='/register' element={<Register/>} exact/>
-            <Route path='/admin' element={<Admin/>} exact/>
-            <Route path='/users' element={<Detail/>} exact/>
-            <Route path='/education/:id' element={<Education/>} exact/>
-            <Route path='/employment/:id' element={<Employment/>} exact/> 
-            <Route path='/Personal/:id' element={<Personal/>} exact/> 
-=======
           <Route path="/" element={<Home />} exact />
           <Route path="/register" element={<Register />} exact />
-
+          <Route path="/admin" element={<Admin />} exact />
           <Route path="/users" element={<Detail />} exact />
           <Route path="/education/:id" element={<Education />} exact />
           <Route path="/employment/:id" element={<Employment />} exact />
-          <Route path="/form" element={<Form />} exact />
->>>>>>> 877c2a09935b9b92b7a10225ddf60a76a5102963
+          <Route path="/Personal/:id" element={<Personal />} exact />
         </Routes>
       </main>
     </div>
