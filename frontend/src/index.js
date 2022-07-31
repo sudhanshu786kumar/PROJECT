f9c067379d628040773edd3abe8850bc7813ee4d
Unnati Moralwar <unnatimoralwar@gmail.com>
2022-07-31 15:24:27 +0530

    styling is done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,30 +1,16 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-
+import { Provider } from "react-redux";
+import { store } from "./store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App'
-import { Provider } from 'react-redux';
-import { store } from './store';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-   <Provider store={store}>
-     <App /></Provider>
-   
-   
->>>>>>> e51564a0dbb94e135462378cc18190ccaba99141
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
